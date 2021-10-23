59caec89b4ac403189e26960e4d9a459cea78fd8
arfento <arfento@gmail.com>
2021-10-24 04:02:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alquran_kotlin/MasjidActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alquran_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MasjidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_masjid)
+    }
+}
